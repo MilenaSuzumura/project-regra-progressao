dffb766213cfdd75d65b7b3a9bc99a828dc73579
milenasuzumura <milena.suzumura11@gmail.com>
2024-04-18 20:45:58 -0300

    Tentei fazer a atividade 1

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,25 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner scanMenu = new Scanner(System.in);
+    String[] arrayNomeAtividades = new String [20];
+    int[] arrayPesoAtividade = new int[20];
+
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+
+    short qntAtividade = scanMenu.nextShort();
+
+    for (int atividade = 1; atividade <= qntAtividade; atividade++) {
+      System.out.println("Digite o nome da atividade " + atividade + ":");
+
+      int index = atividade - 1;
+      String nomeAtividade = scanMenu.next();
+      arrayNomeAtividades[index] = nomeAtividade;
+
+      System.out.println("Digite o peso da atividade " + atividade + ":");
+
+      int pesoAtividade = scanMenu.nextInt();
+      arrayPesoAtividade[index] = pesoAtividade;
+    }
   }
 }
