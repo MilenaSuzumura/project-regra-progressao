7bfdea9c6df29c49a2bc29cc5ca19cdcdd867821
milenasuzumura <milena.suzumura11@gmail.com>
2024-04-18 22:31:41 -0300

    Retornei ao que tava na primeira atividade

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -12,71 +12,25 @@ public class App {
    */
   public static void main(String[] args) {
     Scanner scanMenu = new Scanner(System.in);
+    String[] arrayNome = new String [20];
+    int[] arrayPeso = new int[20];
 
-    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    System.out.println("Digite a quantidade de atividades para cadastrar: ");
 
-    short qntAtividade = scanMenu.nextShort();
+    short qnt = scanMenu.nextShort();
 
-    String[] arrayNomeAtividades = new String [qntAtividade];
-    int[] arrayPesoAtividade = new int[qntAtividade];
-    int[] arrayNotaAtividade = new int[qntAtividade];
-
-
-    for (int atividade = 1; atividade <= qntAtividade; atividade++) {
-      System.out.println("Digite o nome da atividade " + atividade + ":");
+    for (int atividade = 1; atividade <= qnt; atividade++) {
+      System.out.println("Digite o nome da atividade " + atividade + ": ");
 
       int index = atividade - 1;
       String nomeAtividade = scanMenu.next();
-      arrayNomeAtividades[index] = nomeAtividade;
+      arrayNome[index] = nomeAtividade;
 
-      System.out.println("Digite o peso da atividade " + atividade + ":");
+      System.out.println("Digite o peso da atividade " + atividade + ": ");
 
       int pesoAtividade = scanMenu.nextInt();
-      arrayPesoAtividade[index] = pesoAtividade;
-
-      System.out.println("Digite a nota obtida para " + nomeAtividade + ":");
-      int notaAtividade = scanMenu.nextInt();
-      arrayNotaAtividade[index] = notaAtividade;
+      arrayPeso[index] = pesoAtividade;
     }
-
-    int totalPesos = calculaPeso(arrayPesoAtividade);
-    if (totalPesos != 100) {
-      System.out.println("A soma dos pesos é diferente de 100!");
-    }
-
-    double notaFinal = calculaNota(arrayNotaAtividade, arrayPesoAtividade, totalPesos);
-
-    if (notaFinal > 85) {
-      System.out.println("Parabéns! Você alcançou " + notaFinal + "%! E temos o prazer de informar que você obteve aprovação!");
-    } else {
-      System.out.println("Lamentamos informar que, com base na sua pontuação alcançada neste período, " + notaFinal + "%, você não atingiu a pontuação mínima necessária para sua aprovação");
-    }
-  }
-
-  private static int calculaPeso(int[] allPesos) {
-    int totalPesos = 0;
-    for (int i = 0; i < allPesos.length; i++) {
-      totalPesos = totalPesos + allPesos[i];
-    }
-
-    return totalPesos;
-  }
-
-  private static double calculaNota(int[] allNotas, int[] allPesos, int totalPesos) {
-    double[] value1 = new double[allPesos.length - 1];
-
-    for (int i = 0; i < allPesos.length; i++) {
-      value1[i] = allPesos[i] * allNotas[i];
-    }
-
-    System.out.println(value1[0]);
-    double value2 = 0;
-
-    for (int i = 0; i < value1.length; i++) {
-      value2 = value2 + value1[i];
-    }
-
-    return value2 / totalPesos;
   }
 
 }
