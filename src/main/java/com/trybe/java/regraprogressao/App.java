d30dfd10d3643350cc975da3d077264b866ddff5
milenasuzumura <milena.suzumura11@gmail.com>
2024-04-18 23:39:07 -0300

    Fiz o calculo da nota final e fiz a comparação para saber a porcentagem

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -17,6 +17,7 @@ public class App {
     short qnt = Short.parseShort(scanMenu.nextLine());
 
     int pesoTotal = 0;
+    int notaTotal = 0;
 
     for (int atividade = 1; atividade <= qnt; atividade++) {
       System.out.println("Digite o nome da atividade " + atividade + ": ");
@@ -28,11 +29,22 @@ public class App {
 
       System.out.println("Digite a nota obtida para " + nomeAtividade + ":");
       int notaAtividade = Integer.parseInt(scanMenu.nextLine());
+      notaTotal = notaTotal + notaAtividade;
     }
 
     if (pesoTotal != 100) {
       System.out.println("A soma dos pesos é diferente de 100!");
-      scanMenu.close();
+    }
+
+    int notaFinalCima = pesoTotal * notaTotal;
+    int notaFinal = notaFinalCima / pesoTotal;
+
+    if (notaFinal >= 85) {
+      System.out.println("Parabéns! Você alcançou " +
+          notaFinal + "%! E temos o prazer de informar que você obteve aprovação!");
+    } else {
+      System.out.println("Lamentamos informar que, com base na sua pontuação alcançada neste período," +
+          notaFinal + "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
     }
 
     scanMenu.close();
