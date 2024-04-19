5b33fdea58d074371ab1cbdb00257273e52faaf9
milenasuzumura <milena.suzumura11@gmail.com>
2024-04-18 23:45:36 -0300

    adicionei mais System.out.print pra caber a frase toda2

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -40,12 +40,12 @@ public class App {
     int notaFinal = notaFinalCima / pesoTotal;
 
     if (notaFinal >= 85) {
-      System.out.println("Parabéns! Você alcançou " + notaFinal + "%!");
-      System.out.print(" E temos o prazer de informar que você obteve aprovação!");
+      System.out.print("Parabéns! Você alcançou " + notaFinal + "%!");
+      System.out.println(" E temos o prazer de informar que você obteve aprovação!");
     } else {
-      System.out.println("Lamentamos informar que,");
+      System.out.print("Lamentamos informar que,");
       System.out.print("com base na sua pontuação alcançada neste período," + notaFinal + "%,");
-      System.out.print(" você não atingiu a pontuação mínima necessária para sua aprovação.");
+      System.out.println(" você não atingiu a pontuação mínima necessária para sua aprovação.");
     }
 
     scanMenu.close();
